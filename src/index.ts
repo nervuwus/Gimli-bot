75221961ff5b86e5185ad7336896b2cb3a851b22
Merytek <asp3kt47@gmail.com>
2021-08-15 00:07:10 +0200

    Ajout Déclarations V13

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import Client from "./Client";
+import { Intents } from "discord.js";
 
-new Client().init();
+new Client({
+    allowedMentions: { parse: ['users', 'roles'], repliedUser: true },
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+}).init();
