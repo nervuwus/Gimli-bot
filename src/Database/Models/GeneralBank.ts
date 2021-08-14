27d0c4fe3499f4536232c4a1c05d7b0bcbb615f0
Merytek <asp3kt47@gmail.com>
2021-08-15 00:08:16 +0200

    Ajout d'un nouveau model

---
+++ src/Database/Models/GeneralBank.ts
@@ -0,0 +1,13 @@
+import { Document, model, Schema } from "mongoose";
+
+export interface AllBank {
+    bankAmount: number,
+    identifier: string
+}
+
+export const AllBankAmount = new Schema({
+    bankAmount: { type: Number, default: 0},
+    identifier: String
+})
+
+export default model<AllBank>("TotalBankAmount", AllBankAmount);
