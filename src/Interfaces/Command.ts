d72337fb5fd8e2e8a9ad9eca5d32422b5a7f417a
Nervous Nightingale <nightingale@nervous.localdomain>
2021-12-17 22:32:05 +0100

    retrait du any type pour un type plus consistant

--- src/Interfaces/Command.ts
+++ src/Interfaces/Command.ts
@@ -6,7 +6,23 @@ interface execute {
 };
 
 export interface Command {
-    categorie: String,
-    data: any,
+    categorie: string,
+    data: {
+        name: string,
+        type: number,
+        description: string,
+        options?: Array<{
+            name: string,
+            description: string,
+            type: number,
+            required?: boolean,
+            choices?: Array<{
+                name: string,
+                value: string | number
+            }>,
+            autocomplete?: boolean
+        }>
+
+    },
     run: execute
 }
